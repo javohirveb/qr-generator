c524192240445fceb09093c5ceb69c5348f775fa
javohirveb <javohirtoxtasinov0001@gmail.com>
2023-02-17 01:42:37 +0500

    add download

--- js/app.js
+++ js/app.js
@@ -1,13 +1,14 @@
 const wrapper = document.querySelector('.wrapper'),
-qrInput = wrapper.querySelector('.form input'),
-generateBtn = wrapper.querySelector('.form button'),
-qrImg = wrapper.querySelector('.qr-code img')
+   qrInput = wrapper.querySelector('.form input'),
+   generateBtn = wrapper.querySelector('.form button'),
+   qrImg = wrapper.querySelector('.qr-code img')
+const download = document.getElementById('download')
 
 const showInformation = () => {
    let qrValue = qrInput.value
-   if(!qrValue) return
+   if (!qrValue) return
 
-   generateBtn.innerHTML = 'Generating QR Code'
+   generateBtn.innerHTML = 'Generating QR Code...'
 
    qrImg.src = `https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=${qrValue}`
    qrImg.addEventListener('load', () => {
@@ -16,6 +17,13 @@ const showInformation = () => {
    })
 
    wrapper.classList.add('active')
+
+   download.textContent = "Download"
+   download.href = `https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=${qrValue}`
+   download.download = ""
+   download.target = "_blank"
+   // console.log(download);
+   wrapper.appendChild(download)
 }
 
 generateBtn.addEventListener('click', () => {
@@ -24,13 +32,12 @@ generateBtn.addEventListener('click', () => {
 
 document.addEventListener('keydown', (e) => {
    if (e.key == "Enter") {
-       showInformation()
+      showInformation()
    }
 })
 
 qrInput.addEventListener('keyup', () => {
-   if(!qrInput.value) {
+   if (!qrInput.value) {
       wrapper.classList.remove('active')
    }
 })
-
