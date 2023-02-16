494af2feb9479f7e7a1949e572248e2f3690e642
javohirveb <javohirtoxtasinov0001@gmail.com>
2023-02-16 23:27:28 +0500

    initial commit

---
+++ js/app.js
@@ -0,0 +1,36 @@
+const wrapper = document.querySelector('.wrapper'),
+qrInput = wrapper.querySelector('.form input'),
+generateBtn = wrapper.querySelector('.form button'),
+qrImg = wrapper.querySelector('.qr-code img')
+
+const showInformation = () => {
+   let qrValue = qrInput.value
+   if(!qrValue) return
+
+   generateBtn.innerHTML = 'Generating QR Code'
+
+   qrImg.src = `https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=${qrValue}`
+   qrImg.addEventListener('load', () => {
+      wrapper.classList.add('active')
+      generateBtn.innerHTML = 'Generate QR Code'
+   })
+
+   wrapper.classList.add('active')
+}
+
+generateBtn.addEventListener('click', () => {
+   showInformation()
+})
+
+document.addEventListener('keydown', (e) => {
+   if (e.key == "Enter") {
+       showInformation()
+   }
+})
+
+qrInput.addEventListener('keyup', () => {
+   if(!qrInput.value) {
+      wrapper.classList.remove('active')
+   }
+})
+
